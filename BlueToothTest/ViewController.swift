f6f2a52f61c63110a9203c003f425826a821a77b
MJ <terminal201467@gmail.com>
2022-01-26 10:33:26 +0800

    Initial Commit

---
+++ BlueToothTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BlueToothTest
+//
+//  Created by Jhen Mu on 2022/1/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
